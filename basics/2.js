55532c175a123c19bf3b4aca030a2ceef433d329
SANSKAR KASHYAP <106965413+kashyapsanskar@users.noreply.github.com>
2024-09-29 07:20:02 +0000

    container config

---
+++ basics/2.js
@@ -0,0 +1,12 @@
+const accountId = 14453 //cant be changed
+let accountEmail="sanskar@google.com"//let and var are the same thing but use let only
+var accountPassword="12345"
+accountCity="Jaipur"
+let accountState;
+
+// accountId=2 //not allowed
+accountEmail="hc@h.com"
+accountPassword="3213123"
+accountCity="Bengaluru"
+console.log(accountId);
+console.table([accountId,accountEmail,accountPassword,accountCity,accountState]);
