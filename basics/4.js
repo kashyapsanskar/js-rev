844936b14a0425f02a88c8077dc4349b8ce780f6
SANSKAR KASHYAP <106965413+kashyapsanskar@users.noreply.github.com>
2024-09-29 10:01:16 +0000

    datatypeconversion

---
+++ basics/4.js
@@ -0,0 +1,15 @@
+let score=33
+console.log(typeof score);
+console.log(typeof (score));
+let age="33abc"
+console.log(typeof age);
+console.log(typeof (age));
+ 
+let valueInNumber=Number(age) //string ke value ko number meinc onvvert kr do
+
+console.log(typeof valueInNumber);
+//"33"=>33
+// "33abc"=>NaN
+
+
+
