f525b6a7c7d26c4a803a130e19ab6bc76f141257
SANSKAR KASHYAP <106965413+kashyapsanskar@users.noreply.github.com>
2024-09-29 10:08:25 +0000

    Operations

---
+++ basics/5.js
@@ -0,0 +1,20 @@
+// let value=3
+// let negValue=-value
+// console.log(negValue)
+
+let str1="Hello"
+let str2="hitesh"
+
+let str3=str1+str2
+console.log(str3);
+
+console.log(1+"2");
+console.log("1"+2);
+console.log(1+"2"+1);
+console.log(1+2+"2");
+console.log(true);
+let gameCounter=100
+gameCounter++;
+console.log(gameCounter);
+
+
