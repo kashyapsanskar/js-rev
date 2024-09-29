946c7c670b84e4389118b720bfa9c420b1b98e37
SANSKAR KASHYAP <106965413+kashyapsanskar@users.noreply.github.com>
2024-09-29 09:51:02 +0000

    3

---
+++ basics/3.js
@@ -0,0 +1,19 @@
+"use strict" //treat all JS CODE as newer version
+
+// alert("hello") // we are using node.js not browser
+console.log(3+3); //code readability should be high
+console.log("HITESH");
+let name="sanskar" //string can be double or single quote
+let age=19//number
+let isLoggedIn=false //bool
+//null=>standalone value (empty value not undefined)
+//undefined=>value not assigned
+//synmbol =>unique
+//object
+console.log(typeof age);
+console.log(typeof null); //object
+console.log(typeof undefined); //undefined
+
+
+ 
+
