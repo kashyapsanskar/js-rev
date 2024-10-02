48de50990941a79e6e3e1c654cb6328e0a3d0675
SANSKAR KASHYAP <106965413+kashyapsanskar@users.noreply.github.com>
2024-10-02 10:49:42 +0000

    Array basics

---
+++ arrays/1.js
@@ -0,0 +1,40 @@
+//array
+const myArr= [0,1,2,3,4,5,6,true,"sanskar"]
+// console.log(myArr);
+console.log(myArr[3]);
+const myHero=["s","n"];
+const myArr2=new Array(1,2,3,4);
+//array methods
+myArr.push(6);
+console.log(myArr2);
+console.log(myArr);
+
+// myArr.pop();
+myArr2.unshift(9);
+myArr2.shift();
+console.log(myArr2);
+// read documentations
+const newArr=myArr2.join()
+console.log(myArr2)
+
+console.log(newArr);
+
+console.log("A",myArr2);
+const myn1=myArr2.slice(1,3);
+console.log(myn1);
+console.log("B",myArr2)
+const myn2=myArr2.splice(1,3);
+console.log(myn1);
+console.log("B",myArr2)
+ //doubt between slice and splice
+
+
+
+
+
+
+
+
+
+
+
