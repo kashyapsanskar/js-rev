fe60092f4715ea3ba9fbe4a80826180839af98fa
SANSKAR KASHYAP <106965413+kashyapsanskar@users.noreply.github.com>
2024-10-02 06:36:25 +0000

    basics

---
+++ basics/7.js
@@ -0,0 +1,11 @@
+const score=400
+console.log(score);
+const balance=new Number(100) //more properties and methods in this initialisation
+console.log(balance);
+console.log(balance.toString().length); 
+console.log(balance.toFixed(1));
+// maths
+console.log()
+
+
+
