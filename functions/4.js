06897aa80314582a6c180f328b0e7bec23895716
SANSKAR KASHYAP <106965413+kashyapsanskar@users.noreply.github.com>
2024-10-03 07:00:06 +0000

    functions scope mini hoisting

---
+++ functions/4.js
@@ -0,0 +1,32 @@
+//scope
+function one(){
+    const username="Sansksar"
+    function two(){
+        const website="yt"
+        console.log(username);
+        
+    }
+    // console.log(website);
+    // two()
+}
+// one()
+// if (true) {
+//     const username="Sanskar"
+//     if(username=="Sanskar"{
+//         const website="youtube"
+
+// console.log(username+websiteebsite);
+//     }
+// console.log((website));)
+    
+// }
+addone(5)
+console.log(addone(5));
+function  addone(num){
+    return num+1
+}
+addTwo(5)
+const addTwo=function(num){
+    return num+2
+} //this is showing errror because in the different way of function initialization where the function add one and addtwo are intialized in a differente 
+//way
