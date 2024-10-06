89a58170597207223419a615664a5fea242eeb94
SANSKAR KASHYAP <106965413+kashyapsanskar@users.noreply.github.com>
2024-10-06 08:31:14 +0000

    while/dowhile

---
+++ loops/2.js
@@ -0,0 +1,28 @@
+
+// let index = 0
+// while (index <= 10) {
+//     console.log(`Value of index is ${index}`);
+//     index = index + 2
+// }
+// let i=0;
+// while(i<=10){
+//     console.log(`value of index is ${i}`);
+//     i=i+2
+
+
+// }
+
+let my = ['flash', "batman", "superman"]
+
+let i = 0
+while (i < my.length) {
+    console.log(`Value is ${my[i]}`);
+    i = i + 1
+}
+
+let score = 11
+
+do {
+    console.log(`Score is ${score}`);
+    score++
+} while (score <= 10);
